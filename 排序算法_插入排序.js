ed26df9a1559adaeeee65d22f975d896c42fc3a2
zhangyifei233 <15204944127@163.com>
2020-04-28 15:35:20 +0800

    feat 69_x的平方根

--- 排序算法_插入排序.js
+++ 排序算法_插入排序.js
@@ -12,16 +12,16 @@ var sort = function (arr) {
                 }
             }
         }
-        console.log(arr)
+        console.log(arr);
     }
     return arr;
-}
+};
 let arr = [1, 4, 3, 5, 2];
 // for (let index = 0; index < 100; index++) {
 //     arr.push(Math.floor(Math.random() * 101))
 // }
-console.log("数组长度:" + arr.length);
+console.log('数组长度:' + arr.length);
 
 console.time('程序用时');
-console.log(sort(arr))
+console.log(sort(arr));
 console.timeEnd('程序用时');
--- 排序算法_选择排序.js
+++ 排序算法_选择排序.js
@@ -1,12 +1,12 @@
 function sort(arr) {
-    for (var i = 0; i < arr.length-1; i++) {
+    for (var i = 0; i < arr.length - 1; i++) {
         var min = i;
-        for (var j = i+1; j < arr.length; j++) {
-            if(arr[j]<arr[min]){
+        for (var j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[min]) {
                 min = j;
             }
         }
-        if(i!=min){
+        if (i != min) {
             var tmp = arr[i];
             arr[i] = arr[min];
             arr[min] = tmp;
@@ -17,9 +17,9 @@ function sort(arr) {
 
 let arr = [];
 for (let index = 0; index < 10; index++) {
-    arr.push(Math.floor(Math.random()*101))
+    arr.push(Math.floor(Math.random() * 101))
 }
-console.log("数组长度:"+arr.length);
+console.log("数组长度:" + arr.length);
 
 console.time('选择排序用时');
 sort(arr);
