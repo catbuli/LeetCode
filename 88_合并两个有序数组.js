c1ba500ec7a5a497ec689a4fe9e40b137043f5e1
zhangyifei233 <15204944127@163.com>
2020-05-05 16:40:49 +0800

    update 88_合并两个有序数组

--- 88_合并两个有序数组.js
+++ 88_合并两个有序数组.js
@@ -10,12 +10,12 @@ var merge = function (nums1, m, nums2, n) {
     var l2 = n - 1;
     var flag = m + n - 1;
     while (l1 >= 0 && l2 >= 0) {
-        nums1[flag--] = nums1[l1] > nums2[l2] ? nums1[l1--] : nums2[l2--]
+        nums1[flag--] = nums1[l1] > nums2[l2] ? nums1[l1--] : nums2[l2--];
     }
-    nums1.splice(0, l2 + 1, ...nums2.slice(0, l2 + 1))
+    nums1.splice(0, l2 + 1, ...nums2.slice(0, l2 + 1));
 };
 
 console.time('冒泡排序用时');
-console.log(merge([4, 0, 0, 0, 0, 0], 1, [1, 2, 3, 5, 6], 5))
+console.log(merge([4, 0, 0, 0, 0, 0], 1, [1, 2, 3, 5, 6], 5));
 
 console.timeEnd('冒泡排序用时');
