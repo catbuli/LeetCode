7009cf6399c0bd82e52be6758ffdec0e6b160c85
zhangyifei233 <15204944127@163.com>
2020-05-08 12:49:03 +0800

    update 94_二叉树的中序遍历 迭代法

--- 树/94_二叉树的中序遍历.js
+++ 树/94_二叉树的中序遍历.js
@@ -1,28 +1,65 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
- */
+// /**
+//  * Definition for a binary tree node.
+//  * function TreeNode(val) {
+//  *     this.val = val;
+//  *     this.left = this.right = null;
+//  * }
+//  */
+// /**
+//  * @param {TreeNode} root
+//  * @return {number[]}
+//  */
+// var inorderTraversal = function (root) {
+//     var list = [];
+
+//     function recursive(root) {
+//         if (root) {
+//             if (root.left) {
+//                 recursive(root.left)
+//             }
+//             list.push(root.val)
+//             if (root.right) {
+//                 recursive(root.right)
+//             }
+//         }
+//     }
+//     recursive(root);
+//     return list;
+// };
+
+
+// Definition
+// for a binary tree node.
+function TreeNode(val) {
+    this.val = val;
+    this.left = this.right = null;
+}
+
+var list = new TreeNode(1);
+list.right = new TreeNode(2);
+list.right.left = new TreeNode(3);
+
+
 /**
  * @param {TreeNode} root
  * @return {number[]}
  */
 var inorderTraversal = function (root) {
     var list = [];
-
-    function recursive(root) {
-        if (root) {
-            if (root.left) {
-                recursive(root.left)
-            }
-            list.push(root.val)
-            if (root.right) {
-                recursive(root.right)
-            }
+    var stack = [];
+    while (root || stack.length) {
+        while (root) {
+            stack.push(root);
+            root = root.left;
         }
+        root = stack.pop();
+        list.push(root.val);
+        root = root.right;
     }
-    recursive(root);
     return list;
 };
+
+
+console.time('程序用时');
+console.log(inorderTraversal(list));
+console.timeEnd('程序用时');
