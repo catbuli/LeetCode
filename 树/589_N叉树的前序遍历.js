b5ec52e6263548db638dd65c3ed95d1cceda8864
zhangyifei233 <15204944127@163.com>
2020-05-08 09:50:15 +0800

    update 589新增迭代法

--- 树/589_N叉树的前序遍历.js
+++ 树/589_N叉树的前序遍历.js
@@ -1,3 +1,30 @@
+// /**
+//  * // Definition for a Node.
+//  * function Node(val, children) {
+//  *    this.val = val;
+//  *    this.children = children;
+//  * };
+//  */
+
+// /**
+//  * @param {Node} root
+//  * @return {number[]}
+//  */
+// var preorder = function (root) {
+//     var list = [];
+
+//     function recursive(root) {
+//         if (root) {
+//             list.push(root.val);
+//             root.children.forEach(element => {
+//                 recursive(element);
+//             });
+//         }
+//     }
+//     recursive(root);
+//     return list;
+// };
+
 /**
  * // Definition for a Node.
  * function Node(val, children) {
@@ -12,21 +39,15 @@
  */
 var preorder = function (root) {
     var list = [];
-
-    function recursive(root) {
-        if (root) {
-            list.push(root.val);
-            root.children.forEach(element => {
-                recursive(element);
-            });
+    var stack = [];
+    if (!root) return list;
+    stack.push(root)
+    while (stack.length) {
+        var flag = stack.pop();
+        list.push(flag.val);
+        for (let i = flag.children.length - 1; i >= 0; i--) {
+            stack.push(flag.children[i])
         }
     }
-    recursive(root);
     return list;
 };
-
-
-
-console.time('程序用时');
-console.log(preorder(arr));
-console.timeEnd('程序用时');
