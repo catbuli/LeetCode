f97a0d88557e26a94f1dd82b68cb898abf1388a5
zhangyifei233 <15204944127@163.com>
2020-05-08 14:24:41 +0800

    feat 102_二叉树的层序遍历

--- 树/102_二叉树的层序遍历.js
+++ 树/102_二叉树的层序遍历.js
@@ -1,14 +1,38 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
- */
+// function TreeNode(val) {
+//     this.val = val;
+//     this.left = this.right = null;
+// }
+
+// var list = new TreeNode(1);
+// list.left = new TreeNode(2);
+// list.right = new TreeNode(3);
+// list.left.left = new TreeNode(4);
+// list.left.right = new TreeNode(5);
+// var list = [];
 /**
  * @param {TreeNode} root
  * @return {number[][]}
  */
 var levelOrder = function (root) {
-
+    if (!root) return [];
+    var list = [];
+    var queue = [root];
+    var level = 0;
+    while (queue.length) {
+        list.push([]);
+        var length = queue.length;
+        while (length--) {
+            var flag = queue.shift();
+            list[level].push(flag.val)
+            flag.left && queue.push(flag.left);
+            flag.right && queue.push(flag.right);
+        }
+        level++;
+    }
+    return list;
 };
+
+
+console.time('程序用时');
+console.log(levelOrder(list));
+console.timeEnd('程序用时');
