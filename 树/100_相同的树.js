e677b657a1aeb96291d51ee62e4ca81a4c8eafd3
zhangyifei233 <15204944127@163.com>
2020-05-10 20:42:18 +0800

    feat 100_相同的数

---
+++ 树/100_相同的树.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function (p, q) {
+    if (!p && !q) return true;
+    if (!p || !q) return false;
+    if (p.val != q.val) return false;
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
+
+console.time('程序用时');
+console.log(isSameTree(list));
+console.timeEnd('程序用时');
