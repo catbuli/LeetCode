94ab81d2601090cc4a4c86f1478957cc96d5414e
zhangyifei233 <15204944127@163.com>
2020-05-11 15:11:37 +0800

    update 101_对称二叉树

--- 树/101_对称二叉树.js
+++ 树/101_对称二叉树.js
@@ -9,7 +9,7 @@ list.right = new TreeNode(3);
 list.left.left = new TreeNode(4);
 list.left.right = new TreeNode(5);
 list.right.left = new TreeNode(5);
-list.right.right = new TreeNode(4);
+list.right.right = new TreeNode(3);
 
 // /**
 //  * @param {TreeNode} root
@@ -26,6 +26,25 @@ list.right.right = new TreeNode(4);
 //     return recursive(root, root);
 // };
 
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function (root) {
+    if (!root) return true;
+    var queue = [root.left, root.right];
+    while (queue.length > 0) {
+        var t1 = queue.shift();
+        var t2 = queue.shift();
+        if (!t1 && !t2) continue;
+        if (!t1 || !t2) return false;
+        if (t1.val != t2.val) return false;
+        queue.push(t1.left, t2.right, t1.right, t2.left);
+    }
+
+    return true;
+};
+
 console.time('程序用时');
-console.log(isSymmetric(list));
+console.log(isSymmetric(null));
 console.timeEnd('程序用时');
