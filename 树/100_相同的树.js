83e847f3222fda7e70d40c2bf14f0ab1c5716863
zhangyifei233 <15204944127@163.com>
2020-05-11 13:28:38 +0800

    update

--- 树/100_相同的树.js
+++ 树/100_相同的树.js
@@ -10,11 +10,11 @@
  * @param {TreeNode} q
  * @return {boolean}
  */
-var isSameTree = function (p, q) {
-    if (!p && !q) return true;
-    if (!p || !q) return false;
-    if (p.val != q.val) return false;
-    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+var isSameTree = function(p, q) {
+	if (!p && !q) return true;
+	if (!p || !q) return false;
+	if (p.val != q.val) return false;
+	return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 };
 
 console.time('程序用时');
