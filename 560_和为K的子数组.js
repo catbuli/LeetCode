3b46e088e7d0047db43c5070fdb74dd40e373948
zhangyifei233 <15204944127@163.com>
2020-05-15 17:53:00 +0800

    update 560_和为k的子数组 前缀和解法

--- 560_和为K的子数组.js
+++ 560_和为K的子数组.js
@@ -1,23 +1,49 @@
+// /**
+//  * @param {number[]} nums
+//  * @param {number} k
+//  * @return {number}
+//  */
+// var subarraySum = function (nums, k) {
+//     var flag = 0,
+//         num = 0;
+//     for (let i = 0; i < nums.length; ++i) {
+//         num = 0;
+//         for (let j = i; j < nums.length; ++j) {
+//             num += nums[j]
+//             if (num == k) {
+//                 flag++;
+//             }
+//         }
+//     }
+//     return flag;
+// };
+
 /**
  * @param {number[]} nums
  * @param {number} k
  * @return {number}
  */
 var subarraySum = function (nums, k) {
+    var map = new Map();
     var flag = 0,
         num = 0;
+    map.set(0, 1);
     for (let i = 0; i < nums.length; ++i) {
-        num = 0;
-        for (let j = i; j < nums.length; ++j) {
-            num += nums[j]
-            if (num == k) {
-                flag++;
-            }
+        num += nums[i]
+        if (map.has(num - k)) {
+            flag += map.get(num - k);
         }
+        if (map.has(num)) {
+            map.set(num, map.get(num) + 1);
+        } else {
+            map.set(num, 1)
+        }
+
     }
     return flag;
 };
 
-console.time('冒泡排序用时');
+
+console.time('程序用时');
 console.log(subarraySum([28, 54, 7, -70, 22, 65, -6], 100));
-console.timeEnd('冒泡排序用时');
+console.timeEnd('程序用时');
