e2259c2418594e7e980fe0c4a9a68b33bac8cb31
zhangyifei233 <15204944127@163.com>
2020-05-17 17:29:51 +0800

    feat 108_将有序数组转换为二叉搜索树

---
+++ 树/108_将有序数组转换为二叉搜索树.js
@@ -0,0 +1,16 @@
+function TreeNode(val) {
+    this.val = val;
+    this.left = this.right = null;
+}
+
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var sortedArrayToBST = function (nums) {
+
+};
+
+console.time('程序用时');
+console.log(sortedArrayToBST([-10, -3, 0, 5, 9]));
+console.timeEnd('程序用时');
