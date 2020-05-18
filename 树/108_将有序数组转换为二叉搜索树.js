7a0d8da31d8eaa75ed4188d61f70c12ef61e2bee
zhangyifei233 <15204944127@163.com>
2020-05-18 09:52:56 +0800

    update 108

--- 树/108_将有序数组转换为二叉搜索树.js
+++ 树/108_将有序数组转换为二叉搜索树.js
@@ -1,24 +1,24 @@
 function TreeNode(val) {
-    this.val = val;
-    this.left = this.right = null;
+	this.val = val;
+	this.left = this.right = null;
 }
 
 /**
  * @param {number[]} nums
  * @return {TreeNode}
  */
-var sortedArrayToBST = function (nums) {
-    var build = function (list, l, r) {
-        if (l > r) return null;
-        var mid = l + ((r - l) >> 1);
-        var node = new TreeNode(list[mid]);
-        node.left = build(nums, l, mid - 1)
-        node.right = build(nums, mid + 1, r)
-        return node;
-    }
-    return build(nums, 0, nums.length - 1);
+var sortedArrayToBST = function(nums) {
+	var build = function(list, left, right) {
+		if (left > right) return null;
+		var mid = left + ((right - left) >> 1);
+		var node = new TreeNode(list[mid]);
+		node.left = build(nums, left, mid - 1);
+		node.right = build(nums, mid + 1, right);
+		return node;
+	};
+	return build(nums, 0, nums.length - 1);
 };
 
 console.time('程序用时');
-console.log(sortedArrayToBST([-10, -3, 0, 5, 9]));
+console.log(sortedArrayToBST([ -10, -3, 0, 5, 9 ]));
 console.timeEnd('程序用时');
