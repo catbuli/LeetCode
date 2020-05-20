f648b4881b21cb675b150ee40e2fdb615ca4bc98
zhangyifei233 <15204944127@163.com>
2020-05-20 11:46:21 +0800

    update 111

--- 树/111_二叉树的最小深度.js
+++ 树/111_二叉树的最小深度.js
@@ -1,10 +1,13 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
- */
+// Definition for a binary tree node.
+function TreeNode(val) {
+    this.val = val;
+    this.left = this.right = null;
+}
+
+var node = new TreeNode(1);
+node.left = new TreeNode(2);
+
+
 /**
  * @param {TreeNode} root
  * @return {number}
@@ -13,5 +16,9 @@ var minDepth = function (root) {
     if (root == null) {
         return 0;
     }
-    return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+    return !root.right || !root.left ? minDepth(root.left) + minDepth(root.right) + 1 : Math.min(minDepth(root.left), minDepth(root.right)) + 1;
 };
+
+console.time('程序用时');
+console.log(minDepth(node));
+console.timeEnd('程序用时');
