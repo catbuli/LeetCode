2df5bfc140a1e95d9fc7d06cb3c8f5c43595903a
zhangyifei233 <15204944127@163.com>
2020-05-22 14:44:41 +0800

    fupdate 111

--- 树/111_二叉树的最小深度.js
+++ 树/111_二叉树的最小深度.js
@@ -8,15 +8,36 @@ var node = new TreeNode(1);
 node.left = new TreeNode(2);
 
 
+// /**
+//  * @param {TreeNode} root
+//  * @return {number}
+//  */
+// var minDepth = function (root) {
+//     if (root == null) {
+//         return 0;
+//     }
+//     return !root.right || !root.left ? minDepth(root.left) + minDepth(root.right) + 1 : Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+// };
+
 /**
  * @param {TreeNode} root
- * @return {number}
+ * @return {number[][]}
  */
-var minDepth = function (root) {
-    if (root == null) {
-        return 0;
+var levelOrder = function (root) {
+    if (!root) return [];
+    var queue = [root];
+    var level = 1;
+    while (queue.length) {
+        var length = queue.length;
+        while (length--) {
+            var flag = queue.shift();
+            if (!flag.left && !flag.right) return level;
+            flag.left && queue.push(flag.left);
+            flag.right && queue.push(flag.right);
+        }
+        level++;
     }
-    return !root.right || !root.left ? minDepth(root.left) + minDepth(root.right) + 1 : Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+    return level;
 };
 
 console.time('程序用时');
