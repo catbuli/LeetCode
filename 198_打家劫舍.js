9b7bdf5831e6a2e557f448aaa8ce605d7763be70
zhangyifei233 <15204944127@163.com>
2020-05-29 11:24:26 +0800

    feat 198_打家劫舍

---
+++ 198_打家劫舍.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function (nums) {
+    let dp = [0, nums[0]];
+    for (let i = 2; i <= nums.length; i++) {
+        dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i - 1])
+    }
+    return dp[nums.length];
+};
+
+
+console.time('程序用时');
+console.log(rob([1, 2, 3, 1]));
+console.timeEnd('程序用时');
