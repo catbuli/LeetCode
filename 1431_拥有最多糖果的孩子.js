5b5752594dc42f3fdd790851a749b072267ed20b
zhangyifei233 <15204944127@163.com>
2020-06-01 10:09:01 +0800

    update 1431

--- 1431_拥有最多糖果的孩子.js
+++ 1431_拥有最多糖果的孩子.js
@@ -4,5 +4,13 @@
  * @return {boolean[]}
  */
 var kidsWithCandies = function (candies, extraCandies) {
-
+    let max = Math.max(...candies);
+    return candies.map((element) => {
+        return (element + extraCandies) >= max;
+    });
 };
+
+
+console.time('程序用时');
+console.log(kidsWithCandies([2, 3, 5, 1, 3], 3));
+console.timeEnd('程序用时');
