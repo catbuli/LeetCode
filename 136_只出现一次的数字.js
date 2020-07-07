bef67866e9f3e87900347badea4977b09b3165c1
zhangyifei233 <15204944127@163.com>
2020-07-07 14:36:26 +0800

    update 136

--- 136_只出现一次的数字.js
+++ 136_只出现一次的数字.js
@@ -4,28 +4,39 @@
  * @Author: Zhangyifei
  * @Date: 2020-07-07 11:30:09
  * @LastEditors: Zhangyifei
- * @LastEditTime: 2020-07-07 14:14:13
+ * @LastEditTime: 2020-07-07 14:28:33
  * @FilePath: \amr_vuec:\Users\82727\Desktop\LeetCode\136_只出现一次的数字.js
  */
+
+// /**
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// var singleNumber = function (nums) {
+//     let map = new Map();
+//     nums.forEach(element => {
+//         if (map.has(element)) {
+//             map.delete(element)
+//         } else {
+//             map.set(element, 1)
+//         }
+//     });
+//     for (var [key, value] of map) {
+//         return key;
+//     }
+// };
 
 /**
  * @param {number[]} nums
  * @return {number}
  */
 var singleNumber = function (nums) {
-    let map = new Map();
-    nums.forEach(element => {
-        if (map.has(element)) {
-            map.delete(element)
-        } else {
-            map.set(element, 1)
-        }
-    });
-    for (var [key, value] of map) {
-        return key;
+    let flag = 0;
+    for (let i = 0; i < nums.length; i++) {
+        flag ^= nums[i];
     }
+    return flag;
 };
-
 
 console.time('程序用时');
 console.log(singleNumber([1, 2, 1]));
