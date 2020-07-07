b2fd0fd406f90e122c056086581770beabcfb806
zhangyifei233 <15204944127@163.com>
2020-07-07 11:30:52 +0800

    feat 136_只出现一次的数字

---
+++ 136_只出现一次的数字.js
@@ -0,0 +1,22 @@
+/*
+ * @Description: 
+ * @version: 0.0.1
+ * @Author: Zhangyifei
+ * @Date: 2020-07-07 11:30:09
+ * @LastEditors: Zhangyifei
+ * @LastEditTime: 2020-07-07 11:30:22
+ * @FilePath: \amr_vuec:\Users\82727\Desktop\LeetCode\136_只出现一次的数字.js
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function (nums) {
+
+};
+
+
+console.time('程序用时');
+console.log(singleNumber([1, 2, 3, 1]));
+console.timeEnd('程序用时');
