5b5fd8d22157e754dc5b6631f5083fa9c894a06b
zhangyifei233 <15204944127@163.com>
2020-07-13 16:50:52 +0800

    update 3

--- 3_无重复字符的最长子串.js
+++ 3_无重复字符的最长子串.js
@@ -4,46 +4,47 @@
  * @Author: Zhangyifei
  * @Date: 2020-07-10 11:09:12
  * @LastEditors: Zhangyifei
- * @LastEditTime: 2020-07-13 15:41:21
+ * @LastEditTime: 2020-07-13 16:46:56
  * @FilePath: \amr_vuec:\Users\82727\Desktop\LeetCode\3_无重复字符的最长子串.js
  */
 
-// /**
-//  * map
-//  * @param {string} s
-//  * @return {number}
-//  */
-// var lengthOfLongestSubstring = function (s) {
-//     let map = new Map();
-//     let max = 0;
-//     for (let i = 0; i < s.length; i++) {
-//         if (map.has(s[i])) {
-//             map.clear();
-//         }
-//         map.set(s[i], 0)
-//         max = Math.max(map.size, max);
-//     }
-//     return max;
-// };
-
 /**
- * 数组
+ * 哈希表
  * @param {string} s
  * @return {number}
  */
 var lengthOfLongestSubstring = function (s) {
-    let arr = [],
-        max = 0;
-    for (let i = 0; i < s.length; i++) {
-        if (arr.indexOf(s[i]) >= 0) {
-            arr.splice(0, arr.indexOf(s[i]) + 1);
+    let map = new Map();
+    let max = s.length > 0 ? 1 : 0;
+    for (let i = 0, j = 0; i < s.length; i++) {
+        if (map.has(s[i])) {
+            j = map.get(s[i]) + 1;
+            console.log(j, '')
         }
-        arr.push(s[i])
-        max = Math.max(arr.length, max);
+        max = Math.max(max, i - j + 1);
+        map.set(s[i], i)
     }
     return max;
 };
 
+// /**
+//  * 数组
+//  * @param {string} s
+//  * @return {number}
+//  */
+// var lengthOfLongestSubstring = function (s) {
+//     let arr = [],
+//         max = 0;
+//     for (let i = 0; i < s.length; i++) {
+//         if (arr.indexOf(s[i]) >= 0) {
+//             arr.splice(0, arr.indexOf(s[i]) + 1);
+//         }
+//         arr.push(s[i])
+//         max = Math.max(arr.length, max);
+//     }
+//     return max;
+// };
+
 console.time('程序用时');
-console.log(lengthOfLongestSubstring("abcabcbb"));
+console.log(lengthOfLongestSubstring("au"));
 console.timeEnd('程序用时');
